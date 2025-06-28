b35ef0abc5b0183d8ddd6004b2d56209a89aa05c
6shixun <2113453732@qq.com>
2025-06-28 11:40:07 +0800

    first commit

--- day1.py
+++ day1.py
@@ -1,1 +1,10 @@
-print("hello world")
+name = "KUN"  # 字符串
+age = 25        # 整数
+height = 1.75   # 浮点数
+is_student = False  # 布尔值
+
+# 查看变量类型
+print(type(name))   # <class 'str'>
+print(type(age))    # <class 'int'>
+print(type(height)) # <class 'float'>
+print(type(is_student)) # <class 'bool'>
